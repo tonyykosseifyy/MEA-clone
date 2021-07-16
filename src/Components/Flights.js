1e8c078a0e4da46556755c668d2bc1e098917659
tonyy kosseigyy <tony kosseify>
2021-07-16 15:34:10 +0300

    swiper js config

--- src/Components/Flights.js
+++ src/Components/Flights.js
@@ -1,14 +1,16 @@
-import React, { useState } from 'react' ;
+import React, { useState , createRef } from 'react' ;
 import './Flights.css' ;
-import SwiperCore, { Navigation, Pagination, A11y } from 'swiper';
+import SwiperCore, { Navigation, Pagination, A11y , Thumbs } from 'swiper';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper.scss';
 import 'swiper/components/navigation/navigation.scss';
 import 'swiper/components/pagination/pagination.scss';
+import 'swiper/components/effect-fade/effect-fade.scss';
 import { Button } from './About' ;
+import styled from 'styled-components' ;
 
-SwiperCore.use([Navigation , Pagination]);
+SwiperCore.use([Navigation , Pagination, A11y , Thumbs]);
 
 const flights = [
   {
@@ -17,8 +19,8 @@ const flights = [
     travel : 'Travel between: 03 Jul 2021 to 31 Dec 2021' ,
     type: 'Bussiness' ,
     traveltype: 'Round Trip' ,
-    price: 'From EUR 960 *' ,
-    timeleft: 'Fare expires In: 181 days'
+    price: 'EUR 960 *' ,
+    timeleft: '181 days'
   } ,
   {
     title: 'Beirut-Dusseldorf',
@@ -26,8 +28,8 @@ const flights = [
     travel : 'Travel between: 03 Jul 2021 to 31 Dec 2021' ,
     type: 'Economy' ,
     traveltype: 'Round Trip' ,
-    price: 'From USD 460 *' ,
-    timeleft: 'Fare expires In: 181 days'
+    price: 'USD 460 *' ,
+    timeleft: '181 days'
   },
   {
     title: 'Beirut-Istanbul',
@@ -35,8 +37,8 @@ const flights = [
     travel : 'Travel between:  09 Jun 2021 to 31 May 2022' ,
     type: 'Economy' ,
     traveltype: 'Round Trip' ,
-    price: 'From USD 270 *' ,
-    timeleft: 'Fare expires In: 303 days'
+    price: 'USD 270 *' ,
+    timeleft: '303 days'
   },
   {
     title: 'Amsterdam-Beirut',
@@ -44,8 +46,8 @@ const flights = [
     travel : 'Travel between: 21 Jul 2021 to 28 Oct 2021' ,
     type: 'Economy' ,
     traveltype: 'Round Trip' ,
-    price: 'From USD 458 *' ,
-    timeleft: 'Fare expires In: 117 days'
+    price: 'USD 458 *' ,
+    timeleft: '117 days'
   } ,
   {
     title: 'Beirut-Amsterdam',
@@ -53,43 +55,53 @@ const flights = [
     travel : 'Travel between: 21 Jul 2021 to 28 Oct 2021' ,
     type: 'Economy' ,
     traveltype: 'Round Trip' ,
-    price: 'From USD 481 *' ,
-    timeleft: 'Fare expires In: 117 days'
+    price: 'USD 481 *' ,
+    timeleft: '117 days'
   }
 ]
 
 const Flights = () => {
-  const [ grabbing , setGrabbing ] = useState(false) ;
-  const swiperProps = {
-    onDragStart: () => console.log('drag start'),
-    onDragEnd: () => console.log('drag end'),
-    style: {
-      cursor :grabbing ? 'grabbing' : 'grab'
-    }
-  }
-
+  const [ indexx , setIndexx ] = useState(0) ;
   return (
     <div className='flights'>
       <h1>Our latest flight deals</h1>
       <div className='slider'>
-        <div className='slider-sidebar'></div>
+        <div className='slider-sidebar'>
+          <Pointer indexx={indexx} ></Pointer>
+          { flights.map((item , index) => (
+            <PaginationElement
+              active={ index === indexx}
+              onClick={() => {
+                setIndexx(index) ;
+              }}
+              key={index}
+              >
+              {item.title}
+            </PaginationElement>
+          ))}
+        </div>
         <Swiper
           spaceBetween={0}
           slidesPerView={1}
           navigation
           loop
           grabCursor={true}
-          pagination={{ clickable: true }}
+          effect={ window.innerWidth < 1130 ? "fade" : ''}
+          onSlideChange={(e) => setIndexx(e.activeIndex)}
+          autoplay={{delay: 5000}}
         >
           { flights.map((item , index) => (
             <SwiperSlide key={index} >
               <img src={item.url} alt={item.title} />
               <div className='slider-details'>
-                <p>{item.timeleft}</p>
+                <p style={{fontWeight: '200', fontSize: '15px'}}>Fare expires In: <strong>{item.timeleft}</strong></p>
                 <p>{item.travel}</p>
                 <h1>{item.title}</h1>
                 <span>{item.traveltype}</span>
-                <p>{item.price}<span></span></p>
+                <p>From
+                  <strong style={{fontSize:'22px'}}> {item.price.slice(0,3)} </strong>
+                  <strong style={{fontSize:'35px'}}>{item.price.slice(3,-1)} *</strong>
+                </p>
                 <div className='slider-book'>
                   <Button
                     bg='white'
@@ -99,7 +111,7 @@ const Flights = () => {
                     bc='white'
                     c='#0873BA'
                     >Book</Button>
-                  <span>fare conditions</span>
+                  <span>Fare Conditions</span>
                 </div>
               </div>
             </SwiperSlide>
@@ -110,6 +122,33 @@ const Flights = () => {
   ) ;
 } ;
 
+const PaginationElement = styled.p`
+  display: flex ;
+  color: ${props => props.active ? 'white' : '#EFEFEF'} ;
+  cursor: pointer ;
+  transition: .3s ease-out ;
+  font-size: 20px ;
+  margin: 30px 0 ;
+  width: 200px ;
+  font-weight: ${props => props.active ? '400' : '200'} ;
+  text-align: right ;
+  margin-left: auto ;
+  &:hover {
+    color: white ;
+  }
+`
 
+const Pointer = styled.div`
+  position: absolute ;
+  top: ${props => 105 + (props.indexx * 54)}px ;
+  right: 0 ;
+  background-color: #0873BA ;
+  width: 15px ;
+  height: 15px ;
+  border-radius: 50% ;
+  border: 2.5px solid white ;
+  transition: .3s ease-out ;
+  transform: translateX(50%) ;
+`
 
 export default Flights ;
